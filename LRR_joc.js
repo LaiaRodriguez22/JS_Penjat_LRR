6dc94c22011224190d54e6d9f604064201783820
Laia <laia.rodriguez@enti.cat>
2023-11-08 23:34:17 +0100

    08/11 -- Falta implementar tot el tema DOM que ni santa idea i aixo de localStorage per estadistiques.

--- LRR_joc.js
+++ LRR_joc.js
@@ -1,10 +1,14 @@
 /* VARIABLES GLOBALS */
 
-let partidesJugades=0;
-let partidesGuanyades=0;
-let partidesGuanyadesPerc=0;
-let partidesPerdudes=0;
-let partidesPerdudesPerc=0;
+let partidesJugades = 0;
+let partidesGuanyades = 0;
+let partidesGuanyadesPerc = 0;
+let partidesPerdudes = 0;
+let partidesPerdudesPerc = 0;
+
+let paraulaSecreta;
+let intentsFallits;
+let lletresFallides;
 
 function inputUser(){
     let inputUser;
@@ -22,7 +26,7 @@ function inputUser(){
             break;
         } else if (inputUser === 3) {
             // EXIT
-            console.log("exit");
+            exitGame();
             break; 
         } else {
             // TORNEM A PREGUNTAR
@@ -34,7 +38,81 @@ function inputUser(){
 /* APARTAT JOC */
 
 function jocInit(){
-    console.log(" start game");
+
+    //CONTADORS I RESET VARIABLES. 
+    partidesJugades++;
+    intentsFallits = 0;
+    lletresFallides = [];
+
+    // PARAULA QUE ENTRA ADMIN
+    paraulaSecreta = prompt("Entra LA paraula");
+
+    // PARAULA TALLADA
+    let paraulaActual = Array(paraulaSecreta.length).fill("_");
+
+    while (true) {
+        
+        // Mostrem l'estat actual del joc
+        mostraParaulaActual(paraulaActual);
+
+        // Demanem una lletra al jugador
+        let lletra = demanaLletra();
+
+        // Comprovem si la lletra és correcta
+        let correcta = lletraCorrecta(paraulaSecreta, lletra);
+
+        // Actualitzem l'estat del joc
+        if (correcta) {
+            actualitzaParaulaActual(paraulaActual, lletra);
+        } else {
+            lletresFallides.push(lletra);
+        }
+
+        // Mostrem les lletres fallides
+        mostraLletresFallides(lletresFallides);
+
+        // Comprovem si el joc ha acabat
+        if (acabat(paraulaActual, intentsFallits)) {
+            break;
+        }
+    }
+}
+
+function mostraParaulaActual(paraulaActual) {
+    console.log("Paraula actual:", paraulaActual.join(" "));
+}
+
+function demanaLletra() {
+    let lletra = prompt("Entra una lletra:");
+    while (lletra.length !== 1 || !lletra.match(/[a-zA-Z]/)) {
+        lletra = prompt("La lletra ha de tenir una lletra.");
+    }
+    return lletra;
+}
+
+function lletraCorrecta(paraulaSecreta, lletra) {
+    let correcta = paraulaSecreta.includes(lletra);
+    if (!correcta) {
+        intentsFallits++;
+    }
+    return correcta;
+}
+
+function actualitzaParaulaActual(paraulaActual, lletra) {
+    for (let i = 0; i < paraulaSecreta.length; i++) {
+        if (paraulaSecreta[i] === lletra) {
+            paraulaActual[i] = lletra;
+        }
+    }
+}
+
+function mostraLletresFallides(lletresFallides) {
+    console.log("Lletres fallades:", lletresFallides.join(", "));
+    console.log("Intents: " + intentsFallits + "/6");
+}
+
+function acabat(paraulaActual, intentsFallits) {
+    return paraulaActual.join("") === paraulaSecreta || intentsFallits >= 6;
 }
 
 /* APARTAT ESTADISTICA */
